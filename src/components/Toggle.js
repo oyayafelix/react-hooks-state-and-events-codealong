3a8be02b9a13d31b6c31f8d9dbef355ffb7f95b3
oyayafelix <oyayafelix@gmail.com>
2022-08-01 14:28:21 +0300

    Done

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false)
+  const color = isOn ? "red" : "white"
+
+  function handleClick(){
+    setIsOn(!isOn)
+  }
+
+  return <button style = {{background: color}} onClick={handleClick}>{isOn ? 'On' : 'OFF'}</button>;
 }
 
 export default Toggle;
